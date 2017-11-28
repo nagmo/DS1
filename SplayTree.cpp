a2b4947122852fd9950d72eabca785262bff8c09
nagmo <nevoagmon@gmail.com>
2017-11-28 20:10:46 +0200

    in work

--- SplayTree.cpp
+++ SplayTree.cpp
@@ -1,5 +1,113 @@
-//
-// Created by Nevo Agmon on 27/11/2017.
-//
 
 #include "SplayTree.h"
+
+/************************************************************************
+
+ NOTE: the element T must support the following:
+        copy Constructor
+        operators overloading: ==, <, >
+
+
+ ************************************************************************/
+
+/**
+ * no data constructor
+ * @tparam T
+ */
+template <class T>
+SplayTree<T>::SplayTree() : left(nullptr), right(nullptr), data(nullptr) {}
+
+/**
+ * constructor with data initiation.
+ * @tparam T
+ * @param data
+ */
+template <class T>
+SplayTree<T>::SplayTree(T& data) : left(nullptr), right(nullptr), data(
+        nullptr) {
+    this->data = new T(data);
+}
+
+/**
+ * destructor deletes data and deletes each subtree recursively.
+ * @tparam T
+ */
+template <class T>
+SplayTree<T>::~SplayTree(){
+    delete data;
+    delete left;
+    delete right;
+}
+
+/**
+ * insert to the tree, compares data to the data stored in the tree
+ * and places the data in the right place.
+ * to use this function the element <T> must support <,> operators.
+ * @tparam T
+ * @param newData
+ */
+template <class T>
+void SplayTree<T>::Insert(T& newData) {
+    if(data < newData){
+        if(left == nullptr) {
+            left = new SplayTree(newData);
+        }
+        else {
+            left->Insert(newData);
+        }
+    }else if(data > newData){
+        if(right == nullptr) {
+            left = new SplayTree(newData);
+        }
+        else {
+            right->Insert(newData);
+        }
+    }else{
+        throw TreeElementAllreadyInTreeException();
+    }
+}
+
+template <class T>
+T& SplayTree<T>::Find(T& itemToFind){
+
+}
+
+/**
+ * recursive method to splay the tree based on the value given.
+ * searches the item and splaying on the path,
+ * if the item was found it will be at the root at the end,
+ * if not it's father will be at the root
+ * (the user must check at the end of the method).
+ * @tparam T
+ * @param itemToFind
+ * @param father
+ * @param grendpa
+ */
+template <class T>
+void SplayTree<T>::Splay(T& itemToFind, ParentWas father,
+                         ParentWas grendpa){
+    //stop condition: if the item was found or
+    // there are no node to proceed.
+    if(this->data == itemToFind){
+        //rotate
+        return;
+    }else if(this->data < itemToFind && this->right == nullptr){
+        //rotate
+        return;
+    }else if(this->data > itemToFind && this->left == nullptr){
+        //rotate
+        return;
+    }
+    //recursively splay to the subtree needed.
+    if(this->data < itemToFind){
+        this->right->Splay(itemToFind, right, father);
+    }else{
+        this->left->Splay(itemToFind, left, father);
+    }
+    //rotate
+}
+
+
+
+
+
--- SplayTree.h
+++ SplayTree.h
@@ -2,28 +2,74 @@
 #ifndef EX1_SPLAYTREE_H
 #define EX1_SPLAYTREE_H
 
+#include "utility.h"
+
+
+/************************************************************************
+
+ NOTE: the element T must support the following:
+        copy Constructor
+        operators overloading: ==, <, >
+
+
+ ************************************************************************/
+
+
+
 template <class T>
 class SplayTree {
 
 public:
 
+    /**
+    * no data constructor
+    * @tparam T
+    */
     SplayTree();
+
+    /**
+    * constructor with data initiation.
+    * @tparam T
+    * @param data
+    */
+    explicit SplayTree(T&);
+
+    /**
+    * destructor deletes data and deletes each subtree recursively.
+    * @tparam T
+    */
     ~SplayTree();
+
+    /**
+    * insert to the tree, compares data to the data stored in the tree
+    * and places the data in the right place.
+    * to use this function the element <T> must support <,> operators.
+    * @tparam T
+    * @param newData
+    */
     void Insert(T&);
+
+
     void Delete(T&);
     T& Find(T&);
 
 private:
 
-    SplayTree* left;
-    SplayTree* right;
-    T& data;
+    SplayTree *left;
+    SplayTree *right;
+    T* data;
 
-    void Splay();
+    enum ParentWas {left, right, root};
+
+    void Splay(T&, ParentWas, ParentWas);
     void Split();
     void Join();
 
 };
 
+class TreeInvalidInputException : InvalidInputException {};
+class TreeFailureException : FailureException {};
+class TreeElementAllreadyInTreeException : exception{};
+class TreeElementNotInTreeException : exception{};
 
 #endif //EX1_SPLAYTREE_H
