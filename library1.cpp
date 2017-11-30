5a82a36c121ee7583142b12b1d5b1d39b45717eb
nagmo <nevoagmon@gmail.com>
2017-11-30 11:35:28 +0200

    c to cpp functions - finished

--- library1.cpp
+++ library1.cpp
@@ -127,7 +127,19 @@ StatusType LevelUp(void *DS, int gladiatorID, int levelIncrease){
 *                FAILURE - If gladiatorID isn't in the DS, or upgradedID is in the DS.
 *                SUCCESS - Otherwise.
 */
-StatusType UpgradeGladiator(void *DS, int gladiatorID, int upgradedID);
+StatusType UpgradeGladiator(void *DS, int gladiatorID, int upgradedID){
+    if(!DS) return INVALID_INPUT;
+    try {
+        ((ComodosDS*)DS)->UpgradeGladiator(gladiatorID,upgradedID);
+    }catch (InvalidInputException&){
+        return INVALID_INPUT;
+    }catch (FailureException&){
+        return FAILURE;
+    }catch (std::bad_alloc&){
+        return ALLOCATION_ERROR;
+    }
+    return SUCCESS;
+}
 
 /** Description:   Returns the gladiator with the highest level from trainerID
 * 			If trainerID < 0, returns the top gladiator in the entire DS.
@@ -138,7 +150,19 @@ StatusType UpgradeGladiator(void *DS, int gladiatorID, int upgradedID);
 *                INVALID_INPUT - If DS==NULL, or if gladiatorID == NULL, or if trainerID == 0.
 *                SUCCESS - Otherwise.
 */
-StatusType GetTopGladiator(void *DS, int trainerID, int *gladiatorID);
+StatusType GetTopGladiator(void *DS, int trainerID, int *gladiatorID){
+    if(!DS) return INVALID_INPUT;
+    int id;
+    try {
+        id = ((ComodosDS*)DS)->GetTopGladiator(trainerID);
+    }catch (InvalidInputException&) {
+        return INVALID_INPUT;
+    }catch (std::bad_alloc&){
+        return ALLOCATION_ERROR;
+    }
+    *gladiatorID = id;
+    return SUCCESS;
+}
 
 /** Description:   Returns all the gladiators from trainerID sorted by their level.
 *           			If trainerID < 0, returns all the gladiators in the entire DS sorted by their level.
@@ -151,7 +175,30 @@ StatusType GetTopGladiator(void *DS, int trainerID, int *gladiatorID);
 *                INVALID_INPUT - If any of the arguments is NULL or if trainerID == 0.
 *                SUCCESS - Otherwise.
 */
-StatusType GetAllGladiatorsByLevel(void *DS, int trainerID, int **gladiators, int *numOfGladiator);
+StatusType GetAllGladiatorsByLevel(void *DS, int trainerID,
+                                   int **gladiators, int *numOfGladiator){
+    if(!DS) return INVALID_INPUT;
+    GladByLevel glads;
+    try {
+        glads = ((ComodosDS*)DS)->GetAllGladiatorsByLevel(trainerID);
+    }catch (InvalidInputException&){
+        return INVALID_INPUT;
+    }catch (std::bad_alloc&){
+        return ALLOCATION_ERROR;
+    }
+    //allocate array for gladiators using malloc.
+    *gladiators = (int*)malloc(sizeof(int)*glads.numOfGlads);
+    //check for malloc success
+    if(*gladiators == NULL)
+        return ALLOCATION_ERROR;
+    //if succeeded update number of gladiators.
+    *numOfGladiator = glads.numOfGlads;
+    //update the gladiators array
+    for(int i=0; i<glads.numOfGlads; i++){
+        *gladiators[i] = glads.gladiators[i];
+    }
+    return SUCCESS;
+}
 
 /** Description:   Updates the level of the gladiators where gladiatorID % stimulantCode == 0.
 * 			          For each matching gladiator, multiplies its level by stimulantFactor.
@@ -163,7 +210,17 @@ StatusType GetAllGladiatorsByLevel(void *DS, int trainerID, int **gladiators, in
 *                INVALID_INPUT - If DS==NULL or if stimulantCode < 1 or if stimulantFactor <1
 *                SUCCESS - Otherwise.
 */
-StatusType UpdateLevels(void *DS, int stimulantCode, int stimulantFactor);
+StatusType UpdateLevels(void *DS, int stimulantCode, int stimulantFactor){
+    if(!DS) return INVALID_INPUT;
+    try {
+        ((ComodosDS*)DS)->UpdateLevels(stimulantCode,stimulantFactor);
+    }catch (InvalidInputException&){
+        return INVALID_INPUT;
+    }catch (std::bad_alloc&){
+        return ALLOCATION_ERROR;
+    }
+    return SUCCESS;
+}
 
 
 /** Description:   Quits and deletes the database.
@@ -172,4 +229,8 @@ StatusType UpdateLevels(void *DS, int stimulantCode, int stimulantFactor);
 * Output:        None.
 * Return Values: None.
 */
-void Quit(void** DS);
+void Quit(void** DS){
+    if(!DS || !*DS) return;
+    delete (ComodosDS*)*DS;
+    *DS = NULL;
+}
